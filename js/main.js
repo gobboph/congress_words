c343b545fabae1e642c31d14116f87739c5bc5e5
gobbo <robertogobbetti@gmail.com>
2014-04-24 23:20:09 -0400

    implemented API

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+// Color the whole map grey
 window.onload=function() {
    	// Get the Object by ID
    	var a = document.getElementById("svg");
@@ -14,8 +15,20 @@ window.onload=function() {
 };
 
 
+// Function to show response after api call
+function showResponse (response) {
+    RESPONSE = response;
+    if (this && this.url && (typeof(this.url) == "string")) {
+        var anchor = jQuery("#url");
+        anchor.text(this.url.toString());
+        anchor.attr('href', this.url.toString());
+    }
+    jQuery("#output").text(JSON.stringify(response, null, '  '));
+    //console.log('AVERE');
+}
 
-//trying dictionary
+
+// Dictionary with congress members per state
 var number_of_congressmen = {'WA':9, 'OR':5, 'CA':53, 'NV':3, 'AK':1, 'HI':2,
 'ID':2, 'MT':1, 'WY':1, 'UT':3, 'AZ':8, 'NM':3, 'CO':7, 'ND':1, 'SD':1,
 'NE':3, 'KS':4, 'OK':5, 'TX':32, 'LA':7, 'AR':4, 'MO':9, 'IA':5, 'MN':8,
@@ -31,7 +44,7 @@ function codeWord(word) {
 	$("#typed_word").html(word);
 
 	
-
+	
 	// Get the Object by ID
    	var svgElem = document.getElementById("svg");
    	// Get the SVG document inside the Object tag
@@ -39,7 +52,62 @@ function codeWord(word) {
    	// Get one of the SVG items by class;
    	var svgAllItems = svgDoc.getElementsByClassName("state");
 
+   	
 
+	
+	   	// API call
+	function getData(myCallback){
+   		var query_params = { 'apikey': '2cd8dea668b840f989b145e88cb2be80',
+					 //'per_page': 3,
+		   			 'phrase': word,
+		   			 'sort': 'count desc'
+		 			};
+
+		var endpoint = "http://capitolwords.org/api/phrases/state.json";
+
+		//var data = new Array();
+
+		var options = {
+  			data: query_params,
+  			type: 'GET',
+  			dataType: 'jsonp',
+	  		success: myCallback,
+			error: function(){
+				$("#status").html('There was an error loading the data.');
+			}
+		};
+
+		//var request = jQuery.ajax(endpoint,options).done(showResponse);
+		$.ajax(endpoint,options);
+
+	};
+
+	//var data = new Array();
+
+	function myCallback(jsonData){
+		var data = new Array();
+		//var data = jsonData.results;
+		for (var i=0;i<jsonData.results.length;i++){
+			data.push([jsonData.results[i]['state'],jsonData.results[i]['count']]);
+		};
+		console.log('data '+data.length);
+		console.log(typeof(data[0]));
+		console.log(data[0][0]+' '+data[0][1]);
+		//return data;
+	};
+
+	getData(myCallback);
+
+
+
+
+
+	
+
+
+
+
+	
    	//Setting 3 different colors according to how many congressmen they have
 
 	for (var key in number_of_congressmen){
@@ -53,7 +121,7 @@ function codeWord(word) {
 			//set Colors
 			if (key === svgAllItems[i].id){
 				//console.log('eccolo');
-				console.log(key+' '+svgAllItems[i].id);
+				//console.log(key+' '+svgAllItems[i].id);
 				if (parseInt(number_of_congressmen[key])<6){
 					svgAllItems[i].setAttribute("fill", "#0f0");
 				} else if (5<parseInt(number_of_congressmen[key]) && parseInt(number_of_congressmen[key])<16){
@@ -88,10 +156,6 @@ $("#word-form").on("submit", function(e) {
 });
 
 
-
-
-
-//Function to just color a state
 
 
 
