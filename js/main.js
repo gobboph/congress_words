3faf712da2be48b70990e8d590178e189d8dd3a6
gobbo <robertogobbetti@gmail.com>
2014-05-01 12:20:47 -0400

    number of congressmen updated

--- js/main.js
+++ js/main.js
@@ -29,12 +29,12 @@ function showResponse (response) {
 
 
 // Dictionary with congress members per state
-var number_of_congressmen = {'WA':9, 'OR':5, 'CA':53, 'NV':3, 'AK':1, 'HI':2,
-'ID':2, 'MT':1, 'WY':1, 'UT':3, 'AZ':8, 'NM':3, 'CO':7, 'ND':1, 'SD':1,
-'NE':3, 'KS':4, 'OK':5, 'TX':32, 'LA':7, 'AR':4, 'MO':9, 'IA':5, 'MN':8,
-'WI':8, 'IL':19, 'MI':15, 'IN':9, 'KY':6, 'TN':9, 'AL':7, 'MS':4, 'GA':13,
-'FL':25, 'SC':6, 'NC':13, 'VA':11, 'WV':3, 'OH':18, 'PA':19, 'NY':29,
-'VT':1, 'NH':2, 'ME':2, 'MA':10, 'RI':2, 'CT':5, 'NJ':13, 'DE':1, 'MD':8, 'DC':1};
+var number_of_congressmen = {'WA':10, 'OR':5, 'CA':53, 'NV':4, 'AK':1, 'HI':2,
+'ID':2, 'MT':1, 'WY':1, 'UT':4, 'AZ':9, 'NM':3, 'CO':7, 'ND':1, 'SD':1,
+'NE':3, 'KS':4, 'OK':5, 'TX':36, 'LA':6, 'AR':4, 'MO':8, 'IA':4, 'MN':8,
+'WI':8, 'IL':19, 'MI':14, 'IN':9, 'KY':6, 'TN':9, 'AL':7, 'MS':4, 'GA':14,
+'FL':27, 'SC':7, 'NC':13, 'VA':11, 'WV':3, 'OH':16, 'PA':18, 'NY':27,
+'VT':1, 'NH':2, 'ME':2, 'MA':9, 'RI':2, 'CT':5, 'NJ':12, 'DE':1, 'MD':8, 'DC':1};
 
 //console.log(number_of_congressmen.WA);
 
